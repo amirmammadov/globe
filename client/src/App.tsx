0420972eb2cd4ec6150a2cc574060df6b4753048
amirmammadov <amirmammado@gmail.com>
2024-04-11 19:37:30 +0400

    theme was added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import { createTheme, ThemeProvider, Box } from "@mui/material";
+
+import { useTranslation } from "react-i18next";
+
 const App = () => {
-  return <div>globe app</div>;
+  const [t, i18n] = useTranslation("global");
+  const [mode, setMode] = useState<"light" | "dark">("dark");
+
+  const handleLangChange = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <div>{t("title")}</div>
+        <button onClick={() => handleLangChange("az")}>AZ</button>
+        <button onClick={() => handleLangChange("en")}>EN</button>
+        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+          Change theme
+        </button>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+
+import en from "./lang/en/en.json";
+import az from "./lang/az/az.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: en,
+    },
+    az: {
+      global: az,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
